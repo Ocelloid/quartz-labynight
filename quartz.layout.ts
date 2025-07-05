52a0b8421062f763012ec60bb2a5214dd5c0c62d
Nikita Grebnev <ocelloid@gmail.com>
2025-07-05 15:49:48 +0500

    Quartz sync: Jul 5, 2025, 3:49 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,22 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      sortFn: (a, b) => {
+        if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
+          return b.displayName.localeCompare(a.displayName, undefined, {
+            numeric: true,
+            sensitivity: "base",
+          })
+        }
+
+        if (!a.isFolder && b.isFolder) {
+          return 1
+        } else {
+          return -1
+        }
+      },
+    }),
   ],
   right: [
     Component.Graph(),
